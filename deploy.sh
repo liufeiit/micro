c2ce316784e42c2f891fc48867444912d7f72d53
liufeiit <liufei_it@126.com>
2014-11-04 10:12:46 +0800

    dd

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@
 mvn clean
 mvn install
 echo "copy war..."
-cp web/target/micro-web.war ~/dev/jboss-as-web-7.0.2.Final/standalone/deployments/
+cp web/target/micro-web.war $JBOSS_HOME/standalone/deployments/
+cp manager/target/micro-manager.war JBOSS_HOME/standalone/deployments/
 echo "deploy success!"
 
 #nohup sh standalone.sh > nohup.out 2>&1 &
-sh /home/lf/dev/jboss-as-web-7.0.2.Final/bin/standalone.sh
+sh $JBOSS_HOME/bin/standalone.sh
 
