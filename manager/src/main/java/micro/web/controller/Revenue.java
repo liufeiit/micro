c23d11b38fd6a1d5e83ea347b1612b82950799bc
liufeiit <liufei_it@126.com>
2014-11-16 00:10:10 +0800

    ss

--- manager/src/main/java/micro/web/controller/Revenue.java
+++ manager/src/main/java/micro/web/controller/Revenue.java
@@ -2,12 +2,11 @@ package micro.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import micro.web.WebBase;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import micro.core.service.Result;
-import micro.web.WebBase;
 
 /**
  * 
@@ -21,18 +20,12 @@ public class Revenue extends WebBase {
 	@RequestMapping(value = "/revenue_list.htm")
 	public ModelAndView revenue_list(HttpServletRequest request) {
 		ModelAndView mv = returnView(request, "revenue_list", "收入", "收入列表");
-		Result result = articleCatService.getAllArticleCat(false);
-		mv.addObject("hasCat", result.isSuccess());
-		mv.addObject("catList", result.get("catList"));
 		return mv;
 	}
 	
 	@RequestMapping(value = "/revenue_detail.htm")
 	public ModelAndView revenue_detail(HttpServletRequest request) {
 		ModelAndView mv = returnView(request, "revenue_detail", "收入", "会员收入");
-		Result result = articleCatService.getAllArticleCat(false);
-		mv.addObject("hasCat", result.isSuccess());
-		mv.addObject("catList", result.get("catList"));
 		return mv;
 	}
 }
