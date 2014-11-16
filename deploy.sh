f03b6328efbc4b30f31b9fdb8cd248c07998723d
liufeiit <liufei_it@126.com>
2014-11-16 11:50:56 +0800

    ss

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 mvn clean
 mvn install
 echo "copy war..."
-cp web/target/micro-web.war $JBOSS_HOME/standalone/deployments/
-cp manager/target/micro-manager.war JBOSS_HOME/standalone/deployments/
+#cp web/target/micro-web.war $JBOSS_HOME/standalone/deployments/
+cp manager/target/micro-manager.war $JBOSS_HOME/standalone/deployments/
 echo "deploy success!"
 
 #nohup sh standalone.sh > nohup.out 2>&1 &
