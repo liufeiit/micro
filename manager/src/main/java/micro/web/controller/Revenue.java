4f934e8951b4b8a209ee3b3d5676b3e6d1c74610
liufeiit <liufei_it@126.com>
2014-11-17 16:49:23 +0800

    ss

--- manager/src/main/java/micro/web/controller/Revenue.java
+++ manager/src/main/java/micro/web/controller/Revenue.java
@@ -1,7 +1,10 @@
 package micro.web.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import micro.core.dataobject.UserDO;
 import micro.core.service.PageQuery;
 import micro.core.service.Result;
 import micro.web.WebBase;
@@ -11,12 +14,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import tulip.util.CollectionUtil;
+
 /**
  * 
  * @author 刘飞 E-mail:liufei_it@126.com
  * @version 1.0.0
  * @since 2014年11月15日 下午9:37:55
  */
+@SuppressWarnings("unchecked")
 @Controller
 public class Revenue extends WebBase {
 	
@@ -24,10 +30,20 @@ public class Revenue extends WebBase {
 	public ModelAndView revenue_list(HttpServletRequest request) {
 		ModelAndView mv = returnView(request, "revenue_list", "收入", "收入列表");
 		long userId = NumberUtils.toLong(request.getParameter("uid"), 0L);
+		mv.addObject("selected_uid", userId);
 		int page = NumberUtils.toInt(request.getParameter("page"), 1);
+		if(page > 1) {
+			mv.addObject("prePage", page - 1);
+			mv.addObject("currentPage", page);
+			mv.addObject("nextPage", page + 1);
+		} else {
+			mv.addObject("prePage", 1);
+			mv.addObject("currentPage", 1);
+			mv.addObject("nextPage", 2);
+		}
 		Result result = revenueService.query(userId, new PageQuery(page), false);
-		mv.addObject("hasUser", result.isSuccess());
-		mv.addObject("userList", result.get("userList"));
+		mv.addObject("hasRevenue", result.isSuccess());
+		mv.addObject("revenueList", result.get("userList"));
 		return mv;
 	}
 	
@@ -36,8 +52,14 @@ public class Revenue extends WebBase {
 		ModelAndView mv = returnView(request, "revenue_detail", "收入", "会员收入");
 		long userId = NumberUtils.toLong(request.getParameter("uid"), 0L);
 		Result result = revenueService.query(userId, new PageQuery(1), true);
-		mv.addObject("hasUser", result.isSuccess());
-		mv.addObject("userList", result.get("userList"));
+		boolean success = result.isSuccess();
+		mv.addObject("hasUser", success);
+		if(success) {
+			List<UserDO> userList = (List<UserDO>) result.get("userList");
+			if(!CollectionUtil.isEmpty(userList)) {
+				mv.addObject("user", userList.get(0));
+			}
+		}
 		return mv;
 	}
 }
